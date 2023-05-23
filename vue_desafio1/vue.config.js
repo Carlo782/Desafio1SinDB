01517649d00c970bc36ce10293bc170344408788
Carlo N <c.negroni01@ufromail.cl>
2023-05-23 02:52:46 -0400

    Se agregaron nuevos archivos para que funcione correctamente en dockers

--- vue_desafio1/vue.config.js
+++ vue_desafio1/vue.config.js
@@ -2,3 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    proxy: {
+      '/automoviles': {
+        target: 'http://localhost:8080/', // Reemplaza con la URL de tu backend de Spring
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+};
